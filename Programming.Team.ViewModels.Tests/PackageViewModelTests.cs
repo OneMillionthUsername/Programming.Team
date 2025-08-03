ca31a168e5117196574bd3e55ba62c4f488201cb
OneMillionthUserName <dean.marinov@gmx.at>
2025-08-03 17:47:23 +0200

    add visibility for testing internal methods add default test patterns

--- Programming.Team.ViewModels.Tests/PackageViewModelTests.cs
+++ Programming.Team.ViewModels.Tests/PackageViewModelTests.cs
@@ -6,8 +6,8 @@ using Programming.Team.Core;
 using Programming.Team.Data.Core;
 using Programming.Team.PurchaseManager.Core;
 using Programming.Team.ViewModels.Purchase;
+using System.Runtime.CompilerServices;
 using P = Programming.Team.Core.Purchase;
-
 namespace Programming.Team.ViewModels.Tests;
 
 [TestClass]
@@ -19,11 +19,17 @@ public class PackageViewModelTests
     private Mock<IPurchaseManager<Package, P>>? purchaseManager;
     private Mock<NavigationManager>? navigationManager;
     private Mock<AddPackageViewModel>? addViewModel;
+    private List<P>? purchases;
+    private Guid id;
+    private PackageViewModel? vm_id;
+    private PackageViewModel? vm_entity;
+    private Package? package;
 
-
-	[TestInitialize]
+    [TestInitialize]
     public void TestInitialize()
     {
+        // Initialize the mocks and objects needed for the tests
+        id = Guid.NewGuid();
         serviceProvider = new Mock<IServiceProvider>();
         facade = new Mock<IBusinessRepositoryFacade<Package, Guid>>();
         logger = new Mock<ILogger<EntitiesViewModel<Guid, Package, PackageViewModel, IBusinessRepositoryFacade<Package, Guid>>>>();
@@ -31,12 +37,175 @@ public class PackageViewModelTests
         navigationManager = new Mock<NavigationManager>();
         addViewModel = new Mock<AddPackageViewModel>(facade.Object, logger.Object);
 
-		facade.Setup(f => f.Add(It.IsAny<Package>(),It.IsAny<IUnitOfWork>(), It.IsAny<CancellationToken>()))
-            .Returns((Package p, IUnitOfWork uow, CancellationToken token) => p);
+        serviceProvider.Setup(s => s.GetService(typeof(IBusinessRepositoryFacade<Package, Guid>))).Returns(facade.Object);
+        serviceProvider.Setup(s => s.GetService(typeof(ILogger<EntitiesViewModel<Guid, Package, PackageViewModel, IBusinessRepositoryFacade<Package, Guid>>>))).Returns(logger.Object);
+        serviceProvider.Setup(s => s.GetService(typeof(IPurchaseManager<Package, P>))).Returns(purchaseManager.Object);
 
-	}
-	[TestMethod]
-    public void TestMethod1()
+        purchases = new List<P>
+        {
+            new P 
+            {
+                Id = Guid.NewGuid(),
+                UserId = Guid.NewGuid(),
+                PackageId = Guid.NewGuid(),
+                IsPaid = true,
+                PricePaid = 12.7m,
+                ResumeGenerations = 1,
+                StripeSessionUrl = "Some stripe url",
+                StripePaymentIntentId = "Some payment intent id",
+                IsRefunded = false,
+                RefundDate = null
+            }
+        };
+
+        facade.Setup(f => f.Add(It.IsAny<Package>(), It.IsAny<IUnitOfWork>(), It.IsAny<CancellationToken>()))
+            .Returns<Package, IUnitOfWork, CancellationToken>((p, uow, token) => Task.FromResult(p))
+            .Verifiable(Times.Once);
+
+        vm_id = new PackageViewModel(navigationManager.Object, purchaseManager.Object, logger.Object, facade.Object, id);
+        vm_entity = new PackageViewModel(navigationManager.Object, purchaseManager.Object, logger.Object, facade.Object, 
+            new Package
+            {
+                Id = id,
+                Price = 12.7m,
+                ResumeGenerations = 1,
+                StripePriceId = "Some price Id",
+                StripeProductId = "Some product id",
+                StripeUrl = "Some stripe url",
+                Purchases = purchases,
+                Name = "Some name"
+            });
+
+        vm_entity.Price = 12.7m;
+        vm_entity.ResumeGenerations = 1;
+        vm_entity.StripeProductId = "Some product id";
+        vm_entity.StripePriceId = "Some price Id";
+        vm_entity.StripeUrl = "Some stripe url";
+        vm_entity.Name = "Some name";
+
+
+        package = new Package
+        {
+            Id = id,
+            Price = 12.7m,
+            ResumeGenerations = 1,
+            StripePriceId = "Some price Id",
+            StripeProductId = "Some product id",
+            StripeUrl = "Some stripe url",
+            Purchases = purchases,
+            Name = "Some name"
+        };
+
+    }
+    [TestMethod]
+    public void PackageViewModel_Constructor_SetsPropertiesCorrectly()
     {
+        Assert.IsNotNull(vm_id);
+        Assert.AreEqual(id, vm_id!.Id);
+        Assert.IsNull(vm_id.Price);
+        Assert.AreEqual(0, vm_id.ResumeGenerations);
+        Assert.IsNull(vm_id.StripePriceId);
+        Assert.IsNull(vm_id.StripeProductId);
+        Assert.IsNull(vm_id.StripeUrl);
+        Assert.IsNotNull(vm_id.Purchase);
+
+    }
+    [TestMethod]
+    public void PackageViewModel_Constructor_WithEntity_SetsPropertiesCorrectly()
+    {
+        Assert.AreEqual(package!.Id, vm_entity!.Id);
+        Assert.AreEqual(package.Price, vm_entity.Price);
+        Assert.AreEqual(package.ResumeGenerations, vm_entity.ResumeGenerations);
+        Assert.AreEqual(package.StripePriceId, vm_entity.StripePriceId);
+        Assert.AreEqual(package.StripeProductId, vm_entity.StripeProductId);
+        Assert.AreEqual(package.StripeUrl, vm_entity.StripeUrl);
+        Assert.IsNotNull(vm_entity.Purchase);
     }
 }
+
+[TestClass]
+public class TestAddPackageViewModel : AddPackageViewModel
+{
+    public TestAddPackageViewModel(IBusinessRepositoryFacade<Package, Guid> facade, ILogger<AddEntityViewModel<Guid, Package, IBusinessRepositoryFacade<Package, Guid>>> logger)
+        : base(facade, logger) { }
+
+    public Task<Package> CallConstructEntity() => base.ConstructEntity();
+    public Task CallClear() => base.Clear();
+}
+
+[TestClass]
+public class AddPackageViewModelTests
+{
+    [TestMethod]
+    public async Task CallConstructEntity_CreatesCorrectPackage()
+    {
+        var facade = new Mock<IBusinessRepositoryFacade<Package, Guid>>();
+        var logger = new Mock<ILogger<AddEntityViewModel<Guid, Package, IBusinessRepositoryFacade<Package, Guid>>>>();
+        var vm = new TestAddPackageViewModel(facade.Object, logger.Object);
+
+        // Set properties for the test
+        vm.Price = 12.7m;
+        vm.ResumeGenerations = 1;
+        vm.StripeProductId = "Some product id";
+        vm.StripePriceId = "Some price Id";
+        vm.StripeUrl = "Some stripe url";
+        vm.Name = "Some name";
+
+        // Call the method to construct the entity
+        var package = await vm.CallConstructEntity();
+
+        // Assert that the constructed entity has the expected values
+        Assert.AreEqual(12.7m, package.Price);
+        Assert.AreEqual(1, package.ResumeGenerations);
+        Assert.AreEqual("Some name", package.Name);
+    }
+
+    [TestMethod]
+    public async Task CallClear_ResetsProperties()
+    {
+        var facade = new Mock<IBusinessRepositoryFacade<Package, Guid>>();
+        var logger = new Mock<ILogger<AddEntityViewModel<Guid, Package, IBusinessRepositoryFacade<Package, Guid>>>>();
+        var vm = new TestAddPackageViewModel(facade.Object, logger.Object);
+
+        // Set properties before calling Clear
+        vm.Price = 12.7m;
+        vm.ResumeGenerations = 1;
+        vm.StripeProductId = "Some product id";
+        vm.StripePriceId = "Some price Id";
+        vm.StripeUrl = "Some stripe url";
+        vm.Name = "Some name";
+
+        // Call the method to clear the properties
+        await vm.CallClear();
+
+        // Assert that all properties are reset to their default values
+        Assert.IsNull(vm.Price);
+        Assert.AreEqual(0, vm.ResumeGenerations);
+        Assert.AreEqual(string.Empty, vm.Name);
+    }
+
+    [TestMethod]
+    public void CanAdd_ShouldReturnTrue_WhenPropertiesAreSet()
+    {
+        var facade = new Mock<IBusinessRepositoryFacade<Package, Guid>>();
+        var logger = new Mock<ILogger<AddEntityViewModel<Guid, Package, IBusinessRepositoryFacade<Package, Guid>>>>();
+        var vm = new TestAddPackageViewModel(facade.Object, logger.Object);
+
+        // Set all required properties
+        vm.Price = 12.7m;
+        vm.ResumeGenerations = 1;
+        vm.StripeProductId = "Some product id";
+        vm.StripePriceId = "Some price Id";
+        vm.StripeUrl = "Some stripe url";
+        vm.Name = "Some name";
+
+        // Assert that CanAdd returns true
+        Assert.IsTrue(vm.CanAdd);
+    }
+}
+
+
+
+
+
+
